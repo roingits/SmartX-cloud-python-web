4b7ea2ecf7f4b0a69926739c32df4989e4b9427b
18696232390@163.com <18696232390@163.com>
2019-03-15 22:06:09 +0800

    init project dev

---
+++ smartX/smartx-hystrix/src/main/java/com/smartx/smartx/SmartxHystrixApplication.java
@@ -0,0 +1,13 @@
+package com.smartx.smartx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartxHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartxHystrixApplication.class, args);
+    }
+
+}
---
+++ smartX/smartx-service/src/main/java/com/smartx/dao/User.java
@@ -0,0 +1,20 @@
+package com.smartx.dao;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author Mr.chen
+ * @date 2019年3月15日21:24:30
+ */
+@Getter
+@Setter
+@ToString
+public class User {
+
+    private Integer id;
+
+
+
+}
